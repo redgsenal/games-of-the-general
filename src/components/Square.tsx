db4fbcf626ef22c171f02b07cb1f699e9609e11b
reggie.senal <reggie.senal@sg.tribalworldwide.com>
2024-05-21 15:54:21 +0800

    updates

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,22 +1,21 @@
 import { SquareProps } from "../interfaces/SquareProps";
 import Piece from "./Piece";
 
+// a square can have a piece or not
 const Square = (squareProps: SquareProps) => {
   const bgColor = "bg-" + squareProps.color;
   const pieceProps = squareProps.piece;
+  console.log(squareProps);
   return (
-    <>
-      {/* {console.log("square:", squareProps)} */}
-      <div
-        className={bgColor}
-        data-index={squareProps.index}
-        data-y={squareProps.position.y}
-        data-x={squareProps.position.x}
-        data-id={squareProps.id}
-      >
-        <Piece {...pieceProps}></Piece>
-      </div>
-    </>
+    <div
+      className={bgColor}
+      data-index={squareProps.index}
+      data-y={squareProps.position.y}
+      data-x={squareProps.position.x}
+      data-id={squareProps.id}
+    >
+      <Piece {...pieceProps}></Piece>
+    </div>
   );
 };
 
